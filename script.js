978ea8749cd72efd86a3f48a8b36157e7e70de31
Prit-das <pdas99083@gmail.com>
2025-08-02 21:17:42 +0400

    fixex

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
-let pls = document.querySelector('#plus');
-let popup = document.querySelector('#popup');
+let pls = document.querySelector("#plus")
+let popup = document.querySelector("#popup")
+let container = document.querySelector(".container")
+let weekday = document.querySelector("#weekday")
+let monthDate = document.querySelector("#monthDate")
+let todorecords = {}
 
-pls.addEventListener('click', () =>{
-    popup.classList.toggle('inactive');
+let date = new Date()
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]
+let day = weekdays[date.getDay()]
+let month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "April",
+  "May",
+  "Jun",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+]
+let knowmonth = month[date.getMonth()]
+let monthdate = date.getDate()
+weekday.innerHTML = day
+monthDate.innerHTML = `${knowmonth} ${monthdate}`
 
-    if(pls.classList.contains('fa-circle-plus')){
-        pls.classList.remove('fa-circle-plus');
-        pls.classList.add('fa-circle-minus')
-    }
-    else {
-        pls.classList.remove('fa-circle-minus');
-        pls.classList.add('fa-circle-plus');
-    }
-    
-});
-
-
+pls.addEventListener("click", () => {
+  if (pls.classList.contains("fa-circle-plus")) {
+    pls.classList.remove("fa-circle-plus")
+    pls.classList.add("fa-circle-minus")
+    container.lastChild.remove()
+  } else {
+    pls.classList.remove("fa-circle-minus")
+    pls.classList.add("fa-circle-plus")
+    container.insertAdjacentHTML(
+      "beforeend",
+      `<div id="popup" class="inactive">
+          <input
+            name="task"
+            type="text"
+            maxlength="20"
+            placeholder="input task title"
+          />
+          <div class="">
+            <input name="date" type="time" />
+            <button id="submit">Save</button>
+          </div>
+        </div>`
+    )
+    let submit = document.querySelector("#submit")
+    let inputs = document.querySelectorAll("input")
+    submit.addEventListener("click", () => {
+      inputs.forEach((ele) => {
+        todorecords[ele.name] = ele.value
+      })
+      container.lastChild.remove()
+    })
+  }
+})
