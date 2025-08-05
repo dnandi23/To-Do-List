91e2234b20084b84f12252cc8d4f464ed6f75b29
dnandi23 <debarghan2000@gmail.com>
2025-08-05 23:13:59 +0530

    debugging

--- script.js
+++ script.js
@@ -1,18 +1,139 @@
-let pls = document.querySelector('#plus');
-let popup = document.querySelector('#popup');
+// let pls = document.querySelector("#plus")
+// let popup = document.querySelector("#popup")
+// let container = document.querySelector(".container")
+// let weekday = document.querySelector("#weekday")
+// let monthDate = document.querySelector("#monthDate")
+// let todorecords = {}
 
-pls.addEventListener('click', () =>{
-    popup.classList.toggle('inactive');
+// let date = new Date()
+// let weekdays = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ]
+// let day = weekdays[date.getDay()]
+// let month = [
+//   "Jan",
+//   "Feb",
+//   "Mar",
+//   "April",
+//   "May",
+//   "Jun",
+//   "July",
+//   "Aug",
+//   "Sept",
+//   "Oct",
+//   "Nov",
+//   "Dec",
+// ]
+// let knowmonth = month[date.getMonth()]
+// let monthdate = date.getDate()
+// weekday.innerHTML = day
+// monthDate.innerHTML = `${knowmonth} ${monthdate}`
 
-    if(pls.classList.contains('fa-circle-plus')){
-        pls.classList.remove('fa-circle-plus');
-        pls.classList.add('fa-circle-minus')
-    }
-    else {
-        pls.classList.remove('fa-circle-minus');
-        pls.classList.add('fa-circle-plus');
-    }
-    
-});
+// pls.addEventListener("click", () => {
+//   if (pls.classList.contains("fa-circle-plus")) {
+//     pls.classList.remove("fa-circle-plus")
+//     pls.classList.add("fa-circle-minus")
+//     container.lastChild.remove()
+//   } else {
+//     pls.classList.remove("fa-circle-minus")
+//     pls.classList.add("fa-circle-plus")
+//     container.insertAdjacentHTML(
+//       "beforeend",
+//       `<div id="popup" class="inactive">
+//           <input
+//             name="task"
+//             type="text"
+//             maxlength="20"
+//             placeholder="input task title"
+//           />
+//           <div class="">
+//             <input name="date" type="time" />
+//             <button id="submit">Save</button>
+//           </div>
+//         </div>`
+//     )
+//     let submit = document.querySelector("#submit")
+//     let inputs = document.querySelectorAll("input")
+//     submit.addEventListener("click", () => {
+//       inputs.forEach((ele) => {
+//         todorecords[ele.name] = ele.value
+//       })
+//       container.lastChild.remove()
+//     })
+//   }
+// })
 
 
+
+
+
+
+let pls = document.querySelector("#plus") // প্লাস আইকন
+let container = document.querySelector(".container")
+let weekday = document.querySelector("#weekday")
+let monthDate = document.querySelector("#monthDate")
+let todorecords = {} // কাজগুলো সংরক্ষণ করার জন্য
+
+// তারিখ সেট করা
+let date = new Date()
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let months = ["Jan", "Feb", "Mar", "April", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+
+let day = weekdays[date.getDay()]
+let currentMonth = months[date.getMonth()]
+let currentDate = date.getDate()
+
+weekday.innerHTML = day
+monthDate.innerHTML = `${currentMonth} ${currentDate}`
+
+// প্লাস আইকনে ক্লিক ইভেন্ট
+pls.addEventListener("click", () => {
+  let existingPopup = document.querySelector("#popup")
+
+  // যদি popup আগে থেকেই থাকে, তাহলে সেটাকে সরিয়ে দাও এবং প্লাস আইকন দেখাও
+  if (existingPopup) {
+    existingPopup.remove()
+    pls.classList.remove("fa-circle-minus")
+    pls.classList.add("fa-circle-plus")
+    return
+  }
+
+  // না থাকলে popup তৈরি করো এবং মাইনাস আইকন দেখাও
+  pls.classList.remove("fa-circle-plus")
+  pls.classList.add("fa-circle-minus")
+
+  container.insertAdjacentHTML(
+    "beforeend",
+    `<div id="popup">
+        <input name="task" type="text" maxlength="20" placeholder="Input task title" />
+        <div>
+          <input name="date" type="time" />
+          <button id="submit">Save</button>
+        </div>
+      </div>`
+  )
+
+  // সেভ বাটনে ক্লিক করলে ডেটা নিবে
+  let submit = document.querySelector("#submit")
+  let inputs = document.querySelectorAll("#popup input")
+
+  submit.addEventListener("click", () => {
+    inputs.forEach((input) => {
+      todorecords[input.name] = input.value
+    })
+
+    // টাস্ক কনসোলে দেখাও
+    console.log(todorecords)
+
+    // popup রিমুভ করো
+    document.querySelector("#popup").remove()
+    pls.classList.remove("fa-circle-minus")
+    pls.classList.add("fa-circle-plus")
+  })
+})
